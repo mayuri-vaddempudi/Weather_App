a347966b6c7d74fd3431c700cdfaadb494611bc3
Mayuri Yedlapalli <mayuriyedlapalli@Mayuris-MacBook-Pro.local>
2025-12-16 12:40:54 +0100

    conflicts solved

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const API_KEY = "c62c79ea6988def57c1cd62384db73d4";
 const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
-// DOM ELEMENTS   Mayuri
+// DOM ELEMENTS   
 
 const DOM = {
     searchBtn: document.getElementById("searchBtn"),
